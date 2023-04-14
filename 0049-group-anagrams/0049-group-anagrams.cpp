a8855a49bc88b709ea8e4b87c448d54f166fcd3a
adityaxv <85977985+adityaxv@users.noreply.github.com>
2023-04-15 01:15:57 +0530

    Time: 53 ms (22.05%), Space: 21.8 MB (23.04%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (int i = 0; i < strs.size(); i++) {
+            string temp = strs[i];
+            sort(strs[i].begin(), strs[i].end());
+            mp[strs[i]].push_back(temp);
+        }
+        vector<vector<string>> ans;
+        for (auto i : mp) {
+            vector<string> temp;
+            for (auto k : i.second) {
+                temp.push_back(k);
+            }    
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
+/*
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
         map<vector<int>, vector<string>> mp;
         for (int i = 0; i < strs.size(); i++) {
             vector<int> freq(26);
@@ -20,3 +41,4 @@ public:
         return ans;
     }
 };
+*/
