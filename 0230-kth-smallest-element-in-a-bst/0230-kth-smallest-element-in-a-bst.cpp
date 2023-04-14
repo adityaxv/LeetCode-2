8905cab5b15dda18d1f5c09fc1990be86a332413
adityaxv <85977985+adityaxv@users.noreply.github.com>
2023-04-14 11:09:02 +0530

    Time: 24 ms (29.78%), Space: 24.2 MB (42.37%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -11,21 +11,42 @@
  */
 class Solution {
 public:
-    void tree(TreeNode* root, int k, priority_queue<int, vector<int>, greater<int>>& pq) {
+    void tree(TreeNode* root, int& k, int& ans) {
         if (root == NULL) {
             return;
         }
-        if (root -> left) tree(root -> left, k, pq);
-        if (root -> right) tree(root -> right, k, pq);
-        pq.push(root -> val);
+        if (root -> left) tree(root -> left, k, ans);
+        if (--k == 0) {
+            ans = root -> val;
+            return;
+        }
+        if (root -> right) tree(root -> right, k, ans);
+        
     }
     int kthSmallest(TreeNode* root, int k) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        tree(root, k, pq);
-        k--;
-        while (k--) {
-            pq.pop();
-        }
-        return pq.top();
+        int ans = 0;
+        tree(root, k, ans);
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+//     void tree(TreeNode* root, int k, priority_queue<int, vector<int>, greater<int>>& pq) {
+//         if (root == NULL) {
+//             return;
+//         }
+//         if (root -> left) tree(root -> left, k, pq);
+//         if (root -> right) tree(root -> right, k, pq);
+//         pq.push(root -> val);
+//     }
+//     int kthSmallest(TreeNode* root, int k) {
+//         priority_queue<int, vector<int>, greater<int>> pq;
+//         tree(root, k, pq);
+//         k--;
+//         while (k--) {
+//             pq.pop();
+//         }
+//         return pq.top();
+//     }
+// };
